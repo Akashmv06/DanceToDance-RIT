4ea5bac673ee6620d450eab99be88941a1c8fb57
Akashmv06 <akashmv1997@gmail.com>
2021-06-03 19:24:52 +0530

    commit4

--- Accounts/models.py
+++ Accounts/models.py
@@ -1,6 +1,7 @@
 
 from django.db import models
 from MasterEntry.models import District
+from django_countries.fields import CountryField
 
 
 class StudentModel(models.Model):
@@ -18,16 +19,29 @@ class StudentModel(models.Model):
     student_password=models.CharField("Password",max_length=20,unique=True,null=False) 
     student_reenterpassword=models.CharField("Re-enter password",max_length=20,unique=True,null=False)
     student_district=models.ForeignKey(District,on_delete=models.CASCADE,null=True,verbose_name="District")
-    
+    student_country=CountryField()
     student_regdate=models.DateField("student_regdate",auto_now_add=True,null=False)
-    student_dp=models.ImageField("Profile picture",upload_to='profile',null=False)
+    student_dp=models.ImageField("Profile picture",upload_to='profile')
    
     def __str__(self):
         return self.student_username
         
-       
-        
+SUBSCRIPTION = (
+    ('F' , 'FREE'),
+    ('M' , 'MONTHLY'),
+    ('Y' , 'YEARLY'),
+    )
 
+
+class Subscription(models.Model):
+    Student = models.CharField(max_length=40,verbose_name="Student",null=True)
+    is_pro = models.BooleanField(default=False)
+    paid_date=models.DateField(auto_now_add=True,null=True)
+    pro_expiry_date = models.DateField(null=True, blank=True)
+    subscription_type = models.CharField(max_length=100 , choices=SUBSCRIPTION , default='FREE')     
+        
+    def __str__(self):
+        return self.Student
 
 
 
--- Accounts/views.py
+++ Accounts/views.py
@@ -1,10 +1,18 @@
 
 from django.db.models.fields import AutoField, BigAutoField
-from .models import StudentModel
+from django.urls.conf import include
+from .models import StudentModel,Subscription
+from Administrator.models import Tutor
 from django.http import HttpResponse
-from MasterEntry.models import District
+from MasterEntry.models import District, SubscriptionType
 from django.contrib import messages
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+import stripe
+from django.contrib.auth.models import User
+from datetime import datetime, timedelta
+from django.contrib.auth import authenticate
+
+
 
 
 # Create your views here.
@@ -18,6 +26,10 @@ def selectAllDistrict():
     #return render (request,"Accounts/StudentRegistration.html",{'posts':posts})
 
 def studentRegister(request):
+    if request.session.has_key('student_id'):
+            return redirect("/student/home")
+    elif request.session.has_key('tutor_id'):
+            return redirect("/tutor/profile")
     DistrictRecords=selectAllDistrict()
     if request.method == 'POST':
         p=StudentModel()
@@ -28,7 +40,7 @@ def studentRegister(request):
             
 
         p.student_dob= request.POST.get("dob")
-        p.student_dp= request.POST.get("filename")
+        p.student_dp= request.FILES.get("filename")
         p.student_email= request.POST.get("email")
         p.student_gender= request.POST.get("gender")
         p.student_password= request.POST.get("password")
@@ -36,27 +48,110 @@ def studentRegister(request):
         p.student_reenterpassword= request.POST.get("repeatPassword")
         p.student_username=request.POST.get("username")
         p.save()
-        messages.success(request, 'created successfully.')
+        if request.session.has_key('student_id'):
+            return redirect("/student/home")
+        elif request.session.has_key('tutor_id'):
+            return redirect("/tutor/profile")
+        StudentObj = get_object_or_404(StudentModel, student_username=request.POST.get("username"),student_password=request.POST.get("password"))
+        request.session["student_id"]=StudentObj.id
+        request.session["student_name"]=StudentObj.student_name
+        #messages.success(request, 'created successfully.')
           
-        return render(request, "Accounts/StudentRegistration.html",{'DistrictRecords':DistrictRecords})  
+        #return render(request, "Accounts/StudentRegistration.html",{'DistrictRecords':DistrictRecords})  
+        return redirect("/student/home")
 
     else:
         return render(request, "Accounts/StudentRegistration.html",{'DistrictRecords':DistrictRecords})
 
 
-        #login
-def login(request):
-    if request.method=='POST':
-        StudentDataCount=StudentModel.objects.filter(student_username=request.POST.get("username"),student_password=request.POST.get("password")).count()
-        if StudentDataCount>0:
-            StudendentObj = get_object_or_404(StudentModel, tstudent_username=request.POST.get("username"),student_password=request.POST.get("Password"))
-            request.session["student_id"]=StudendentObj.student_id
-            request.session["student_name"]=StudendentObj.student_name
-            return redirect("/accounts/homepage")
-        else: 
-            return HttpResponse("Invalid Data")
-    return render(request,"Accounts/Login.html",{})
 
-#home
-def home(request):
-    return render(request,"Accounts/Home.html",{})
+        #login
+
+def login(request):
+    if request.session.has_key('student_id'):
+        return redirect("/student/home")
+        
+    elif request.session.has_key('tutor_id'):
+        return redirect("/tutor/profile")
+    
+    else:
+        if request.method=='POST':
+            StudentDataCount=StudentModel.objects.filter(student_username=request.POST.get("username"),student_password=request.POST.get("password")).count()
+            TutorDataCount=Tutor.objects.filter(tutor_username=request.POST.get("username"),tutor_password=request.POST.get("password")).count()
+            if StudentDataCount>0:
+                StudentObj = get_object_or_404(StudentModel, student_username=request.POST.get("username"),student_password=request.POST.get("password"))
+                request.session["student_id"]=StudentObj.id
+                request.session["student_name"]=StudentObj.student_name
+                p=StudentModel.objects.all()
+                return redirect("/student/home",{'p':p})
+            elif TutorDataCount>0:
+                Tutorobj = get_object_or_404(Tutor,tutor_username=request.POST.get("username"),tutor_password=request.POST.get("password"))
+                request.session["tutor_id"]=Tutorobj.id
+                request.session["tutor_name"]=Tutorobj.tutor_name
+                return redirect("/tutor/profile")
+            else: 
+               return render(request,"Accounts/Login.html",{'fail':1})
+        return render(request,"Accounts/Login.html",{})
+
+
+def logout(request):
+    request.session.flush()
+    return redirect("/accounts/login")
+    
+
+def subscription(request):
+    if request.method == 'POST':
+        membership = request.POST.get('membership' , 'MONTHLY')
+        amount = 1000
+        if membership == 'YEARLY':
+            amount = 11000
+        stripe.api_key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
+        sid= request.session['student_id']
+        s = get_object_or_404(StudentModel,id=sid)
+        request.session['student_email']=s.student_email
+        request.session['student_username']=s.student_username
+        customer = stripe.Customer.create(
+            
+            
+            email =request.session['student_email'],
+			name=request.session['student_username'],
+            source=request.POST['stripeToken']
+			)
+            
+        charge = stripe.Charge.create(
+			customer=customer,
+			amount=amount*100,
+			currency='inr',
+			description="Membership",   
+		)
+        
+        print(charge['amount'])
+        if charge['paid'] == True:
+            sid=request.session['student_id']
+            profile=Subscription()
+            profile.Student=sid
+            if charge['amount'] == 100000:
+                
+                profile.subscription_type = 'M'
+                profile.is_pro = True
+                expiry = datetime.now() + timedelta(30)
+                profile.pro_expiry_date = expiry
+                profile.save()
+            elif charge['amount'] == 1100000:
+                profile.subscription_type = 'Y'
+                profile.is_pro = True
+                expiry = datetime.now() + timedelta(365)
+                profile.pro_expiry_date = expiry
+                profile.save()
+        
+        print(charge)
+        return redirect("/accounts/success/")
+    
+            
+   
+    return render(request, 'Accounts/Subscribe.html')
+ 
+
+def charge(request):
+    return render(request, 'Accounts/charge.html')
+        
